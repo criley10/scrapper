6933ac7ee97a1e09b72f842baf3f2e40388386e0
Chris Riley <chrisrex2041@gmail.com>
2018-09-17 15:05:38 -0400

    added screenshot

--- headless/headless.py
+++ headless/headless.py
@@ -2,3 +2,4 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(executable_path=r"C:/Users/Christopher/Downloads/Software/chromedriver_wexe",  chrome_options=chrome_options)
+driver.get_screenshot_as_file("capture.png")
